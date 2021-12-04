6fdf41a59cf73267fd3dcb6b6d00f8fcf0450074
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-12-04 02:30:30 -0300

    creando login con Identity

---
+++ Context/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChallengeDisney.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeDisney.Context
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        private const string Schema = "users";
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema(Schema);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ChallengeDisney.Models
+{
+    public class User : IdentityUser
+    {
+        public bool IsActive { get; set; }
+    }
+}
